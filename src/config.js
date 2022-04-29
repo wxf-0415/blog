f1d2fef2c0f26d3903a903e7d582e8ddc9ed5bc9
wxf <1337675840@qq.com>
2022-04-29 15:03:48 +0800

    1

--- src/config.js
+++ src/config.js
@@ -1,29 +1,29 @@
 export default {
   // page1部分
   page1: {
-    titleEn: 'Hello,I`m WeiTingting', //英文标题
-    title: '一个追梦在前端路上的小白', //中文标题
+    titleEn: 'Hello,I`m wxf', //英文标题
+    title: '个人博客', //中文标题
   },
   // page2部分
   page2: {
-    authorImg: 'page1.jpg', // 作者头像
-    xinhui: '我叫卫婷婷，是一名在读的大三学生。', // 幸会
-    qiuzhi: '前端开发', // 求职意向
-    guanyuwo: '掌握了前端js、html、css基本技术和Vue框架、UI框架，nodejs等等，有着公司项目实战的经验和一些java安卓开发经历。希望可以和大家一起学习，共同进步！' // 关于我
+    authorImg: 'page3.jpg', // 作者头像
+    xinhui: '很荣幸有人前来观看我的个人网页', // 幸会
+    qiuzhi: '读研深造', // 求职意向
+    guanyuwo: '性别男！微信：17738666071' // 关于我
   },
   // page3部分
   page3: [{
     icon: 'icon-tubiao-',
-    title: 'UI设计',
-    msg: ['PS切图、界面排版']
+    title: '网站建设',
+    msg: ['这就是一个试水项目']
   },{
     icon: 'icon-diannao', // 图标
     title: '网页制作', // 标题
-    msg: ['响应式页面', 'css、scss动画效果',] //介绍
+    msg: ['考完研再好好完善', '或者就业？',] //介绍
   }, {
     icon: 'icon-qianbi1',
-    title: '前端功能',
-    msg: ['JS完成常见的交互功能', '用AJAX读取后台数据且渲染']
+    title: '算法设计',
+    msg: ['C++优化', 'python优化', '算法设计']
   },  {
     icon: 'icon-shouji',
     title: '框架',
@@ -35,39 +35,42 @@ export default {
     course: [{
       date: '2020/9——至今', // 时间
       desc: { // 经历
-        title: '山西传媒学院',
-        list: ['山西省职业技能大赛web前端开发一等奖', '专升本上岸', '软件开发公司实习']
+        title: '东北大学',
+        list: ['东北大学优秀学生', '东北大学二等奖学金']
       }
-    }, {
-      date: '2019/9——2020/9',
+    }, 
+    {
+      date: '2019/9——2022/4',
       desc: {
-        title: '晋城职业技术学院',
-        list: ['学习前端语言', '构建页面 / 利用JS、CSS3等制作网站，vue有关项目', '维护508工作室','获得Web前段开发1+X证书','普通话二级甲等','国家励志奖学金']
+        title: '东北大学ACM训练队',
+        list: ['学习C++算法', '辽宁省程序设计金奖', 'CCCC全国大学生程序设计竞赛三等奖']
       }
-    }, {
-      date: '2018/9——2019/9',
+    },
+     {
+      date: '2020/9——2022/3/10',
       desc: {
-        title: '晋城职业技术学院',
-        list: ['学习java语言', '利用Android、sql等制作app', '参加山西省职业院校移动互联技能大赛', '全国计算机等级二级证书', '国家励志奖学金']
+        title: '东北大学大学生创新创业计划',
+        list: ['研究关于海洋时序温度检测科研问题', '以第二作者发表一篇中文核心期刊论文《计算机系统应用》']
       }
-    }],
+    }
+  ],
     // 我的拓展技能掌握
     singlelist: [{
-      title: 'bootsrap',
+      title: 'C++',
       text: '了解'
     }, {
-      title: 'Element-ui',
-      text: '掌握'
+      title: 'Python-ui',
+      text: '了解'
     }, {
-      title: 'mint-ui',
-      text: '掌握'
+      title: '数据结构',
+      text: '了解'
     }, {
-      title: 'scss',
-      text: '掌握'
+      title: '算法',
+      text: '了解'
     }],
     // 我的基本技能掌握
     proresslist: [{
-      title: 'HTML、CSS、SCSS',
+      title: 'C++,HTTP,CSS',
       value: '90%'
     }, {
       title: 'JS、JQ',
@@ -84,44 +87,44 @@ export default {
     }]
   },
   // page5部分
-  page5: [{
-    title: '508工作室',
-    content: '晋城职业技术学院508工作室独立站点的维护',
-    image: 'box1.png',
-    href: 'https://lab508.gitee.io/'
-  }, {
-    title: '图书管理系统',
-    content: 'Vue,NodeJs图书管理系统前后端',
-    image: 'box3.jpg',
-    href: 'https://gitee.com/wttAndroid/book_admin'
-  }, {
-    title: '校园约吧',
-    content: 'Vue,NodeJs移动端校园项目前后端',
-    image: 'box5.jpg',
-    href: 'https://gitee.com/wttAndroid/xyy_server'
-  }, {
-    title: '蘑菇街',
-    content: 'Vue蘑菇街商城案例前端',
-    image: 'box2.png',
-    href: 'https://github.com/wttAndroid/MyShoppingStreet'
-  }, {
-    title: '个人简历',
-    content: 'html,css,jq,bootsrap搭建个人简历网站',
-    image: 'box4.png',
-    href: ''
-  }, {
-    title: '二维码生成器',
-    content: 'Android二维码生成器',
-    image: 'box6.jpeg',
-    href: ''
-  }],
+  // page5: [{
+  //   title: '508工作室',
+  //   content: '晋城职业技术学院508工作室独立站点的维护',
+  //   image: 'box1.png',
+  //   href: 'https://lab508.gitee.io/'
+  // }, {
+  //   title: '图书管理系统',
+  //   content: 'Vue,NodeJs图书管理系统前后端',
+  //   image: 'box3.jpg',
+  //   href: 'https://gitee.com/wttAndroid/book_admin'
+  // }, {
+  //   title: '校园约吧',
+  //   content: 'Vue,NodeJs移动端校园项目前后端',
+  //   image: 'box5.jpg',
+  //   href: 'https://gitee.com/wttAndroid/xyy_server'
+  // }, {
+  //   title: '蘑菇街',
+  //   content: 'Vue蘑菇街商城案例前端',
+  //   image: 'box2.png',
+  //   href: 'https://github.com/wttAndroid/MyShoppingStreet'
+  // }, {
+  //   title: '个人简历',
+  //   content: 'html,css,jq,bootsrap搭建个人简历网站',
+  //   image: 'box4.png',
+  //   href: ''
+  // }, {
+  //   title: '二维码生成器',
+  //   content: 'Android二维码生成器',
+  //   image: 'box6.jpeg',
+  //   href: ''
+  // }],
   // page6部分
   page6: {
-    github: 'https://github.com/wttAndroid',
+    github: 'https://github.com/wxf-0415',
     bili: '',
     email: 'Mailto:1457321681@qq.com?Subject=邮箱标题&Body=邮箱内容！',
     zhihu: '',
-    weixin: "<img style='width:120px' src='http://wttandroid.gitee.io/wttandroid.github.io/src/img/weix.jpg' alt='加载失败'>",
-    qq: "<img style='width:120px' src='http://wttandroid.gitee.io/wttandroid.github.io/src/img/qq.jpg' alt='加载失败'>"
+    weixin: "<img style='width:120px' src='src/img/weixin.png' alt='加载失败'>",
+    qq: "<img style='width:120px' src='src/img/qq.png' alt='加载失败'>"
   }
 }
